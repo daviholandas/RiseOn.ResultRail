1ffd13bd794da289a4bcd87168da64a854251ef8
Davi Holanda <daviholandas@gmail.com>
2025-02-11 21:21:32 -0300

    Refactor Upshot extension methods for consistency and improved type handling

--- src/RiseOn.RailResult.Upshot/Extensions/UpshotExtensions.cs
+++ src/RiseOn.RailResult.Upshot/Extensions/UpshotExtensions.cs
@@ -7,9 +7,7 @@ public static partial class UpshotExtensions
     public static IUpshot OnRail(this IUpshot upshot,
         Func<IUpshot> successRail,
         Func<IUpshot> failRail)
-        => upshot.IsSuccess ?
-            successRail() :
-            failRail();
+        => upshot.IsSuccess ? successRail() : failRail();
 
     public static TR OnRail<T, TR>(this T value,
         Func<T, bool> predicate,
@@ -17,59 +15,29 @@ public static partial class UpshotExtensions
         Func<T, TR> failRail)
         where T : new()
         where TR: new()
-        => predicate(value) ?
-            successRail(value) :
-            failRail(value);
+        => predicate(value) ? successRail(value) : failRail(value);
 
     public static IUpshot OnRail<T>(this T value,
         Func<T, bool> predicate,
         Func<T, IUpshot> successRail,
         Func<T, IUpshot> failRail)
         where T : new()
-        => predicate(value) ?
-            successRail(value) :
-            failRail(value);
+        => predicate(value) ? successRail(value) : failRail(value);
 
-    public static Upshot OnRailSuccess(this Upshot upshot,
-        Action<Upshot> action)
-    {
-        if (upshot.IsSuccess)
-            action(upshot);
+    public static IUpshot OnRailSuccess(this IUpshot upshot,
+        Func<IUpshot> action)
+        => upshot.IsSuccess ? action() : upshot;
 
-        return upshot;
-    }
-    
-    public static Upshot OnRailFail(this Upshot upshot,
-        Action<Upshot> action)
-    {
-        if (upshot.IsFailure)
-            action(upshot);
+    public static IUpshot OnRailFail(this IUpshot upshot,
+        Func<IUpshot> action)
+        => upshot.IsFailure ? action() : upshot;
 
-        return upshot;
-    }
+    public static TR? Map<TR>(this IUpshot upshot,
+        TR? defaultValue,
+        Func<IUpshot, TR> func)
+        => upshot.IsSuccess ? func(upshot) : defaultValue ?? default;
 
-    public static Upshot OnRail(this Upshot upshot,
-        Expression<Func<Upshot, Upshot>> expression)
-    {
-        return upshot.IsSuccess
-            ? expression.Compile().Invoke(upshot)
-            : upshot;
-        //TODO: It's wrong. It doing the same thing of OnRailSuccess
-    }
-
-    public static TR Map<TR>(this Upshot upshot,
-        Func<Upshot, TR> func)
-    {
-        return func(upshot);
-    }
-
-    public static Upshot<TR> Map<TR>(this Upshot upshot,
-        Func<TR> func)
-        where TR : new ()
-    {
-        if(upshot.IsFailure)
-            return Upshot<TR>.Fail(upshot.Message);
-
-        return Upshot<TR>.Success(func());
-    }
+    public static IUpshot Map(this IUpshot upshot,
+        Func<IUpshot> action)
+        => upshot.IsSuccess ? action() : Upshot.Fail(upshot.Error);
 }
--- src/RiseOn.RailResult.Upshot/Extensions/UpshotTypeExtensions.cs
+++ src/RiseOn.RailResult.Upshot/Extensions/UpshotTypeExtensions.cs
@@ -4,135 +4,112 @@ namespace RiseOn.RailResult.Upshot.Extensions;
 
 public static partial class UpshotExtensions
 {
-    public static Upshot<T> OnRail<T>(this T value,
+    public static IUpshot<T> OnRail<T>(this T value,
         Func<T, bool> predicate,
-        Func<T, Upshot<T>> successRail,
-        Func<T, Upshot<T>> failRail)
+        Func<T, IUpshot<T>> successRail,
+        Func<T, IUpshot<T>> failRail)
         where T : new()
-        => predicate(value) ?
-            successRail(value) :
-            failRail(value);
+        => predicate(value) ? successRail(value) : failRail(value);
 
-    public static Upshot<TR> OnRail<T, TR>(this T value,
+    public static IUpshot<TR> OnRail<T, TR>(this T value,
         Func<T, bool> predicate,
-        Func<T, Upshot<TR>> successRail,
-        Func<T, Upshot<TR>> failRail)
+        Func<T, IUpshot<TR>> successRail,
+        Func<T, IUpshot<TR>> failRail)
         where T : new()
         where TR : new()
-        => predicate(value) ?
-            successRail(value) :
-            failRail(value);
+        => predicate(value) ? successRail(value) : failRail(value);
 
     public static T OnRail<T>(this IUpshot<T> upshot,
         Func<IUpshot<T>, T> successRail,
         Func<IUpshot<T>, T> failRail)
         where T : new()
-        => upshot.IsSuccess ?
-            successRail(upshot) :
-            failRail(upshot);
+        => upshot.IsSuccess ? successRail(upshot) : failRail(upshot);
 
     public static TR OnRail<T,TR>(this IUpshot<T> upshot,
         Func<IUpshot<T>, TR> successRail,
         Func<IUpshot<T>, TR> failRail)
         where T : new()
         where TR : new()
-        => upshot.IsSuccess ?
-            successRail(upshot) :
-            failRail(upshot);
+        => upshot.IsSuccess ? successRail(upshot) : failRail(upshot);
 
-    public static Upshot<T> OnRailSuccess<T>(this Upshot<T> upshot,
-        Func<Upshot<T>, Upshot<T>> action)
+    public static IUpshot<T> OnRailSuccess<T>(this IUpshot<T> upshot,
+        Func<IUpshot<T>, IUpshot<T>> action)
         where T : new()
-        => upshot.IsSuccess
-            ? action(upshot)
-            : upshot;
+        => upshot.IsSuccess ? action(upshot) : upshot;
 
-    public static IUpshot OnRailSuccess<T>(this Upshot<T> upshot,
-        Func<Upshot<T>, IUpshot> action)
+    public static IUpshot OnRailSuccess<T>(this IUpshot<T> upshot,
+        Func<IUpshot<T>, IUpshot> action)
         where T : new()
-        => upshot.IsSuccess
-            ? action(upshot)
-            : upshot;
+        => upshot.IsSuccess ? action(upshot) : upshot;
 
-    public static Upshot<TR> OnRailSuccess<T, TR>(this Upshot<T> upshot,
-        Func<Upshot<T>, TR> action)
+    public static IUpshot<TR> OnRailSuccess<T, TR>(this IUpshot<T> upshot,
+        Func<IUpshot<T>, TR> action)
         where T : new()
         where TR : new()
         => upshot.Map(action);
 
-    public static Upshot<TR> OnRailSuccess<T, TR>(this Upshot<T> upshot,
+    public static IUpshot<TR> OnRailSuccess<T, TR>(this IUpshot<T> upshot,
         Func<T, TR> action)
         where T : new()
         where TR : new()
         => upshot.Map(action);
 
-    public static Upshot<TR> OnRailSuccess<T, TR>(this Upshot<T> upshot,
-        Func<T, Upshot<TR>> action)
+    public static IUpshot<TR> OnRailSuccess<T, TR>(this IUpshot<T> upshot,
+        Func<T, IUpshot<TR>> action)
         where T : new()
         where TR : new()
         => upshot.Map(action);
 
-    
-    public static Upshot<T> OnRailFail<T>(this Upshot<T> upshot,
-        Func<Upshot<T>, Upshot<T>> action)
+    public static IUpshot<T> OnRailFail<T>(this IUpshot<T> upshot,
+        Func<IUpshot<T>, IUpshot<T>> action)
         where T : new()
-        => upshot.IsFailure
-            ? action(upshot)
-            : upshot;
+        => upshot.IsFailure ? action(upshot) : upshot;
 
-    public static Upshot<TR> OnRailFail<T, TR>(this Upshot<T> upshot,
-        Func<Upshot<T>, TR> action)
+    public static IUpshot<TR> OnRailFail<T, TR>(this IUpshot<T> upshot,
+        Func<IUpshot<T>, TR> action)
         where T : new()
         where TR : new()
-        => upshot.Map(action);
+        => upshot.IsFailure ? Upshot<TR>.Success(action(upshot)) : Upshot<TR>.Fail(upshot.Error);
 
-    public static Upshot<TR> OnRailFail<T, TR>(this Upshot<T> upshot,
-        Func<T, Upshot<TR>> action)
+    public static IUpshot<TR> OnRailFail<T, TR>(this IUpshot<T> upshot,
+        Func<T, IUpshot<TR>> action)
         where T : new()
         where TR : new()
-        => upshot.Map(action);
+        => upshot.IsFailure ? action(upshot.Value) : Upshot<TR>.Fail(upshot.Error);
 
-    public static Upshot<TR> OnRailFail<T, TR>(this Upshot<T> upshot,
+    public static IUpshot<TR> OnRailFail<T, TR>(this IUpshot<T> upshot,
         Func<T, TR> action)
         where T : new()
         where TR : new()
-        => upshot.Map(action);
+        => upshot.IsFailure ? Upshot<TR>.Success(action(upshot.Value)) : Upshot<TR>.Fail(upshot.Error);
 
-    public static Upshot<TR> Map<T, TR>(this Upshot<T> upshot,
+    public static IUpshot<TR> Map<T, TR>(this IUpshot<T> upshot,
         Func<T, TR> func)
         where TR : new()
         where T : new()
-        => upshot.IsFailure
-            ? Upshot<TR>.Fail(upshot.Error)
-            : Upshot<TR>.Success(func(upshot.Value));
+        => upshot.IsSuccess ? Upshot<TR>.Success(func(upshot.Value)) : Upshot<TR>.Fail(upshot.Error);
 
-    public static Upshot<TR> Map<T, TR>(this Upshot<T> upshot,
-        Func<T, Upshot<TR>> func)
+    public static IUpshot<TR> Map<T, TR>(this IUpshot<T> upshot,
+        Func<T, IUpshot<TR>> func)
         where TR : new()
         where T : new()
-        => upshot.IsFailure
-            ? Upshot<TR>.Fail(upshot.Error)
-            : func(upshot.Value);
-    
-    public static Upshot<TR> Map<T, TR>(this Upshot<T> upshot,
-       Func<Upshot<T>, TR> func)
-       where TR : new()
-       where T : new()
-       => upshot.IsFailure
-            ? Upshot<TR>.Fail(upshot.Error)
-            : Upshot<TR>.Success(func(upshot));
+        => upshot.IsSuccess ? func(upshot.Value) : Upshot<TR>.Fail(upshot.Error);
 
-    public static TR Map<T, TR>(this Upshot<T> upshot,
+    public static IUpshot<TR> Map<T, TR>(this IUpshot<T> upshot,
+        Func<IUpshot<T>, TR> func)
+        where TR : new()
+        where T : new()
+        => upshot.IsSuccess ? Upshot<TR>.Success(func(upshot)) : Upshot<TR>.Fail(upshot.Error);
+
+    public static TR? Map<T, TR>(this IUpshot<T> upshot,
         Func<T, TR> func,
-        TR defaultValue)
+        TR? defaultValue)
         where TR : new()
         where T : new()
-        => upshot.IsFailure
-            ? defaultValue
-            : func(upshot.Value);
+        => upshot.IsSuccess ? defaultValue ?? default : func(upshot.Value);
 
-    public static TR Finally<T, TR>(this Upshot<T> result,
-        Func<Upshot<T>, TR> func)
+    public static TR Finally<T, TR>(this IUpshot<T> result,
+        Func<IUpshot<T>, TR> func)
         where T : new()
         => func(result);
 }
